02560d8bfd1201f42615b9ef9a59020eba08f98b
trzl47 <brownty47@gmail.com>
2017-03-08 00:34:28 -0600

    Completed Footer Component Build

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div>
-				<div> Footer - Social Media Banner </div>
-				<div> Footer - Contact Info </div>
+      <div className="footer container">
+				<div className="row media">
+					<img className="desktop icon" src={require('./assets/logo-color.png')} alt="logo"></img>
+					<a href=""><i className="fa fa-facebook fa-2x"></i><span></span></a>
+					<a href=""><i className="fa fa-twitter fa-2x"></i><span></span></a>
+					<a href=""><i className="fa fa-instagram fa-2x"></i><span></span></a>
+					<span><a href=""> Contact Us </a> | <a href=""> Terms & Conditions </a></span>
+				</div>
+				<div className="row contact">
+					<span> PYT Dance Studio | 14 Bridge STreet, New York, NY 10101 | 212.867.5309 | contact@pytdance.com </span>
+					<br />
+					<span> All Rights Reserved, &copy; 2016 PYT Dance Studio </span>
+				</div>
       </div>
     );
   }
