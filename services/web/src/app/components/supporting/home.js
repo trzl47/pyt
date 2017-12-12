fce491c30f952475e4ca3fca8fae6b2817283abb
trzl47 <brownty47@gmail.com>
2017-12-12 13:53:15 -0600

    home panel styles

--- services/web/src/app/components/supporting/home.js
+++ services/web/src/app/components/supporting/home.js
@@ -3,21 +3,21 @@ import Panel from './content/panels/panel.js';
 
 class HomeSupporting extends Component {
 	render() {
-    let panelContent = this.props.panels;
-    let panels = panelContent.map((panel) => {
-      return <Panel title={panel.title}
+		let panelContent = this.props.panels;
+		let panels = panelContent.map((panel) => {
+			return <Panel title={panel.title}
 										desc={panel.description}
-                    sub={panel.subtext}
-                    key={panel.id}
+										sub={panel.subtext}
+										key={panel.id}
 										img={panel.img}
-									 	color={panel.color}/>
-    });
-    return (
-				<div className="home">
+										color={panel.color}/>;
+		});
+		return (
+				<div className="supporting home">
 					{panels}
 				</div>
-    );
-  }
+		);
+	}
 }
 
 export default HomeSupporting;
