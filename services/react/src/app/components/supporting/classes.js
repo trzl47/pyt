36f3e416d411a4f88b7672d5d768c75705e5547a
trzl <brownty47@gmail.com>
2018-06-13 23:12:02 +0000

    CSSGRID!! javascript no longer needed

--- services/react/src/app/components/supporting/classes.js
+++ services/react/src/app/components/supporting/classes.js
@@ -6,7 +6,8 @@ import '../../static/css/classes-supporting.css';
 const renderClasses = (classes, length) => {
 	const classcards = classes.map((classcard) => {
 		return (
-			<ClassCard key={classcard.id}
+			<ClassCard className={classcard.id.toString()}
+								key={classcard.id}
 								title={classcard.title}
 								weekdays={classcard.weekdays}
 								weekdaysched={classcard.weekdaysched}
@@ -15,26 +16,8 @@ const renderClasses = (classes, length) => {
 								desc={classcard.description} />
 		);
 	});
-	const chunkClasses = (arr, size) => {
-		let rows = [];
-		for (let i = 0; i < arr.length; i+=size) {
-			rows.push(arr.slice(i,i+size));
-		}
-		return rows;
-	};
-	const renderRows = () => {
-		return (
-			chunkClasses(classcards, 3).map((row, index) => {
-				return (
-					<div className="row" key={index}>
-						{ row }
-					</div>
-				);
-			})
-		);
-	};
 	return (
-		renderRows()
+		classcards
 	);
 };
 
