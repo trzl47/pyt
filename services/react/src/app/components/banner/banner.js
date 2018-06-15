265f43260a6b96750fa69d906bec6736dd5164aa
trzl <brownty47@gmail.com>
2018-06-15 00:29:08 +0000

    design spec changes

--- services/react/src/app/components/banner/banner.js
+++ services/react/src/app/components/banner/banner.js
@@ -7,6 +7,7 @@ const renderBanner = (content) => {
 	if (content.page === 'about') {
 		return (
 			<div className="about banner">
+				<img className='about image' src={require('../../static/img/workout-photo.png')} />
 				<div className="about bannerinfo">
 					<h1> ABOUT PYT DANCE </h1>
 					<p>Lorem ipsum dolor sit amet, consectetur<br />adipiscing elit. Curabitur pharetra placerat felis<br />et dignissim. Morbi nec mi leo. Aliquam eget.</p>
@@ -17,6 +18,7 @@ const renderBanner = (content) => {
 	else if (content.page === 'classes') {
 		return (
 			<div className="classes banner">
+				<img className='classes image' src={require('../../static/img/classes-photo.png')} />
 				<div className="classes bannerinfo">
 					<h1> CLASSES </h1>
 					<p>Lorem ipsum dolor sit amet, consectetur<br />adipiscing elit. Curabitur pharetra placerat felis<br />et dignissim. Morbi nec mi leo. Aliquam eget.</p>
@@ -27,6 +29,8 @@ const renderBanner = (content) => {
 	else if (content.page === 'faculty') {
 		return (
 			<div className="faculty banner">
+				<div className="faculty image">
+				</div>
 				<div className="faculty bannerinfo">
 					<h1> FACULTY </h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />Curabitur pharetra placerat felis et dignissim. Morbi nec mi<br />leo. Aliquam eget.</p>
