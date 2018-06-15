701af15b68f692e0979546a94200d1b8c463a30d
trzl <brownty47@gmail.com>
2018-06-15 00:55:19 +0000

    created and pushed new prod image ver 1

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-mkdir services/react-nginx/build && \
-cp -r services/react/build/server services/react-nginx/build && \
-cp -r services/react/build/client services/react-nginx/build && \
-docker build -t trzl/portfolio:augment-react.1 -f ./services/react-nginx/Dockerfile.prod ./services/react-nginx && \
-docker push trzl/portfolio:augment-react.1 && \
-docker build -t trzl/portfolio:augment-apollo.2 -f ./services/apollo/Dockerfile ./services/apollo && \
-docker push trzl/portfolio:augment-apollo.2 && \
-rm -rf services/react-nginx/build && \
-docker system prune
+docker-compose -f docker-compose-build.yml up --build -d && \
+docker system prune --volumes && \
+mkdir services/pyt-nginx/build && \
+cp -r services/react/build/server services/pyt-nginx/build && \
+cp -r services/react/build/client services/pyt-nginx/build && \
+docker build -t trzl/portfolio:pyt-react.1 -f ./services/pyt-nginx/Dockerfile.prod ./services/pyt-nginx && \
+docker push trzl/portfolio:pyt-react.1 && \
+docker-compose down && \
+docker system prune --volumes && \
+sudo rm -rf services/pyt-nginx/build && \
+sudo rm -rf services/react/build/client services/react/build/server && \
 echo "Build complete!"
